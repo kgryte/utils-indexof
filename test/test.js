e4d6b5e8d2c0ec33b314479b911809850c7351d1
kgryte <kgryte@gmail.com>
2016-03-01 11:54:26 -0800

    Add check for array-like first argument

--- test/test.js
+++ test/test.js
@@ -12,3 +12,30 @@ tape( 'main export is a function', function test( t ) {
 	t.equal( typeof indexOf, 'function', 'main export is a function' );
 	t.end();
 });
+
+tape( 'the function throws a type error if not provided an array-like object', function test( t ) {
+	var values;
+	var i;
+
+	values = [
+		// '5', // array-like
+		5,
+		NaN,
+		true,
+		null,
+		undefined,
+		{}, // not array-like, as no length
+		function(){} // has length, but is a function
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		t.throws( badValue( values[i] ), TypeError, 'throws type error when provided ' + values[ i ] );
+	}
+	t.end();
+
+	function badValue( value ) {
+		return function badValue() {
+			indexOf( value, 3 );
+		};
+	}
+});
